0924d9453860ebb96ee306cc22926cad3d97c1c1
Brian Hirsh <hirsheybar@meta.com>
2024-07-22 10:56:42 -0700

    [test] avoid OpOverloadPacket.__getattr__ calls in inductor lowering
    Test Plan: CI
    
    Differential Revision: D60048270

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -210,7 +210,8 @@ def mark_nodes_dislike_padding(g):
     def _get_overload_packet(node):
         return (
             node.target._overloadpacket
-            if node.op == "call_function" and hasattr(node.target, "_overloadpacket")
+            # hasattr on OpOverloadPacket is slow, do isinstance first
+            if node.op == "call_function" and isinstance(node.target, torch._ops.OpOverload) and hasattr(node.target, "_overloadpacket")
             else None
         )
 
@@ -913,7 +914,8 @@ class GraphLowering(torch.fx.Interpreter):
         if target is operator.getitem and isinstance(args[0], (list, tuple, dict)):
             return super().call_function(target, args, kwargs)
 
-        if hasattr(target, "_inductor_lowering_function"):
+        # hasattr on OpOverloadPacket is slow, check isinstance first
+        if not isinstance(target, torch._ops.OpOverloadPacket) and hasattr(target, "_inductor_lowering_function"):
             # passthrough lowerings from .pattern_matcher
             return target(*args, **kwargs)
 
